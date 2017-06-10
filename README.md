cecbd49b229e014a92cc2a14b85d37ceab1686b3
Maxim Mazurok <maxim@mazurok.com>
2017-06-10 16:18:17 +0300

    edition_menu

--- README.md
+++ README.md
@@ -1,8 +1,67 @@
 #### Editions
 
 ##### Add New One 
-    /api?table=editions&type=add&name=Edition 1
+    GET /api
+        ?table=editions
+        &type=add
+        &name=Edition 1
   
 ##### Get All
-    /api?table=editions&type=get
+    GET /api
+        ?table=editions
+        &type=get
 
+#### Edition Menu
+
+##### Add New One
+    POST /api
+        ?table=edition_menu
+        &type=add
+        
+    Request Payload
+    
+    ------WebKitFormBoundarykAdOVRwnUGoIy2QF
+    Content-Disposition: form-data; name="edition_name"
+    
+    edition_name_1
+    ------WebKitFormBoundarykAdOVRwnUGoIy2QF
+    Content-Disposition: form-data; name="model_number"
+    
+    2
+    ------WebKitFormBoundarykAdOVRwnUGoIy2QF
+    Content-Disposition: form-data; name="model_name"
+    
+    model_name_1
+    ------WebKitFormBoundarykAdOVRwnUGoIy2QF
+    Content-Disposition: form-data; name="short_name"
+    
+    short_name_1
+    ------WebKitFormBoundarykAdOVRwnUGoIy2QF
+    Content-Disposition: form-data; name="video_button"; filename="file_to_upload_1.png"
+    Content-Type: image/png
+    
+    
+    ------WebKitFormBoundarykAdOVRwnUGoIy2QF
+    Content-Disposition: form-data; name="subscription_button"; filename="file_to_upload_2.png"
+    Content-Type: image/png
+    
+    
+    ------WebKitFormBoundarykAdOVRwnUGoIy2QF
+    Content-Disposition: form-data; name="image_button"; filename="file_to_upload_3.png"
+    Content-Type: image/png
+    
+    
+    ------WebKitFormBoundarykAdOVRwnUGoIy2QF--
+    
+#### Get All (Except images)
+    GET /api
+        ?table=edition_menu
+        &type=get
+    
+#### Get Image
+    GET /api
+        ?table=edition_menu
+        &type=get
+        &model_number=2
+        &edition_name=edition_name_1
+        &file=image_button
--- api/index.php
+++ api/index.php
@@ -14,6 +14,28 @@ switch ($_GET['type']) {
                 $r = pg_query($db, "SELECT * FROM editions");
                 echo json_encode(pg_num_rows($r) > 0 ? pg_fetch_all($r) : []);
                 break;
+            case 'edition_menu':
+                if (!empty($_GET['file']) && !empty($_GET['edition_name']) && !empty($_GET['model_number'])) {
+                    switch ($_GET['file']) {
+                        case 'video_button':
+                        case 'subscription_button':
+                        case 'image_button':
+                            $r = pg_query($db, "SELECT {$_GET['file']} FROM edition_menu WHERE edition_name = '{$_GET['edition_name']}' AND model_number = '{$_GET['model_number']}'");
+                            $res = pg_fetch_assoc($r);
+                            $data = pg_unescape_bytea($res[$_GET['file']]);
+                            $mime_type = finfo_buffer(finfo_open(), $data, FILEINFO_MIME_TYPE);
+                            header("Content-type: {$mime_type}", true);
+                            echo $data;
+                            break;
+                        default:
+                            die(404);
+                            break;
+                    }
+                } else {
+                    $r = pg_query($db, "SELECT edition_name, model_number, model_name, short_name FROM edition_menu");
+                    echo json_encode(pg_num_rows($r) > 0 ? pg_fetch_all($r) : []);
+                }
+                break;
             default:
                 die(404);
                 break;
@@ -30,6 +52,44 @@ switch ($_GET['type']) {
                             '{$_GET['name']}'
                         )"
                     );
+                }
+                break;
+            case 'edition_menu':
+                $post_fields = array(
+                    'edition_name',
+                    'model_number',
+                    'model_name',
+                    'short_name'
+                );
+                $upload_fields = array(
+                    'video_button',
+                    'subscription_button',
+                    'image_button'
+                );
+
+                $sql_fields = array();
+
+                $fields_not_empty = true;
+                foreach ($post_fields as $field) {
+                    if (empty($_POST[$field])) {
+                        $fields_not_empty = false;
+                    }
+                    array_push($sql_fields, $_POST[$field]);
+                }
+                foreach ($upload_fields as $field) {
+                    if (empty($_FILES[$field])) {
+                        $fields_not_empty = false;
+                    }
+                    $data = file_get_contents($_FILES[$field]['tmp_name']);
+                    if (!$data) {
+                        $fields_not_empty = false;
+                    }
+                    array_push($sql_fields, pg_escape_bytea($data));
+                }
+
+                if ($fields_not_empty) {
+                    $query = "INSERT INTO edition_menu VALUES ('" . join("','", $sql_fields) . "')";
+                    pg_query($db, $query);
                 }
                 break;
             default:
