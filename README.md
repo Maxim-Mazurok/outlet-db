0b7c05e3f83b520f1dd23939f1fc809c097933c4
Maxim Mazurok <maxim@mazurok.com>
2017-06-18 19:51:57 +0300

    images_menu

--- README.md
+++ README.md
@@ -65,3 +65,59 @@
         &model_number=2
         &edition_name=edition_name_1
         &file=image_button
+
+#### Images Menu
+
+##### Add New One
+    POST /api
+        ?table=images_menu
+        &type=add
+        
+    Request Payload
+    
+    ------WebKitFormBoundarykAdOVRwnUGoIy2QF
+    Content-Disposition: form-data; name="edition_name"
+    
+    edition_name_1
+    ------WebKitFormBoundarykAdOVRwnUGoIy2QF
+    Content-Disposition: form-data; name="model_number"
+    
+    2
+    ------WebKitFormBoundarykAdOVRwnUGoIy2QF
+    Content-Disposition: form-data; name="model_name"
+    
+    model_name_1
+    ------WebKitFormBoundarykAdOVRwnUGoIy2QF
+    Content-Disposition: form-data; name="short_name"
+    
+    short_name_1
+    ------WebKitFormBoundarykAdOVRwnUGoIy2QF
+    Content-Disposition: form-data; name="video_button"; filename="file_to_upload_1.png"
+    Content-Type: image/png
+    
+    
+    ------WebKitFormBoundarykAdOVRwnUGoIy2QF
+    Content-Disposition: form-data; name="subscription_button"; filename="file_to_upload_2.png"
+    Content-Type: image/png
+    
+    
+    ------WebKitFormBoundarykAdOVRwnUGoIy2QF
+    Content-Disposition: form-data; name="image_button"; filename="file_to_upload_3.png"
+    Content-Type: image/png
+    
+    
+    ------WebKitFormBoundarykAdOVRwnUGoIy2QF--
+    
+#### Get All (Except images)
+    GET /api
+        ?table=images_menu
+        &type=get
+    
+#### Get Image
+    GET /api
+        ?table=images_menu
+        &type=get
+        &edition_name=edition_name_1
+        &model_number=2
+        &product_id=123
+        &file=image_button
--- api/index.php
+++ api/index.php
@@ -5,6 +5,18 @@ if ($_ENV['PHP_ENV'] !== 'production') {
     $dotenv->load();
 }
 
+ini_set('file_uploads', 'On');
+ini_set('upload_max_filesize', '50M');
+ini_set('post_max_size', '50M');
+
+function not_empty_get(array $items) {
+    foreach ($items as $item) {
+        if (!array_key_exists($item, $_GET)) return false;
+        if (empty($_GET[$item])) return false;
+    }
+    return true;
+}
+
 require_once('../include/db.php');
 
 switch ($_GET['type']) {
@@ -15,7 +27,7 @@ switch ($_GET['type']) {
                 echo json_encode(pg_num_rows($r) > 0 ? pg_fetch_all($r) : []);
                 break;
             case 'edition_menu':
-                if (!empty($_GET['file']) && !empty($_GET['edition_name']) && !empty($_GET['model_number'])) {
+                if (not_empty_get(['file', 'edition_name', 'model_number'])) {
                     switch ($_GET['file']) {
                         case 'video_button':
                         case 'subscription_button':
@@ -36,6 +48,29 @@ switch ($_GET['type']) {
                     echo json_encode(pg_num_rows($r) > 0 ? pg_fetch_all($r) : []);
                 }
                 break;
+            case 'images_menu':
+                if (not_empty_get(['edition_name', 'model_number', 'product_id'])) {
+                    switch ($_GET['file']) {
+                        case 'thumbnail':
+                        case 'subscription_image':
+                        case 'download_image':
+                            $f = $_GET['file'];
+                            $r = pg_query($db, "SELECT $f FROM images_menu WHERE edition_name = '{$_GET['edition_name']}' AND model_number = '{$_GET['model_number']}' AND product_id = '{$_GET['product_id']}'");
+                            $res = pg_fetch_assoc($r);
+                            $data = pg_unescape_bytea($res[$_GET['file']]);
+                            $mime_type = finfo_buffer(finfo_open(), $data, FILEINFO_MIME_TYPE);
+                            header("Content-type: {$mime_type}", true);
+                            echo $data;
+                            break;
+                        default:
+                            die(404);
+                            break;
+                    }
+                } else {
+                    $r = pg_query($db, "SELECT edition_name, model_number, model_name, short_name, product_id, price_gbp, price_usd, price_eur FROM images_menu");
+                    echo json_encode(pg_num_rows($r) > 0 ? pg_fetch_all($r) : []);
+                }
+                break;
             default:
                 die(404);
                 break;
@@ -47,7 +82,7 @@ switch ($_GET['type']) {
             case 'editions':
                 if (!empty($_GET['name'])) {
                     pg_query($db, "
-                        INSERT INTO editions 
+                        INSERT INTO {$_GET['table']} 
                         VALUES (
                             '{$_GET['name']}'
                         )"
@@ -88,7 +123,44 @@ switch ($_GET['type']) {
                 }
 
                 if ($fields_not_empty) {
-                    $query = "INSERT INTO edition_menu VALUES ('" . join("','", $sql_fields) . "')";
+                    $query = "INSERT INTO {$_GET['table']} VALUES ('" . join("','", $sql_fields) . "')";
+                    pg_query($db, $query);
+                }
+                break;
+            case 'images_menu':
+                $post_fields = array(
+                    'edition_name',
+                    'model_number',
+                    'model_name',
+                    'short_name',
+                    '(upl)thumbnail',
+                    '(upl)subscription_image',
+                    '(upl)download_image',
+                    'product_id',
+                    'price_gbp',
+                    'price_usd',
+                    'price_eur'
+                );
+
+                $sql_fields = array();
+
+                $fields_not_empty = true;
+                foreach ($post_fields as $field) {
+                    if (substr($field, 0, 5) === '(upl)') {
+                        $field = substr($field, 5);
+                        if (empty($_FILES[$field])) $fields_not_empty = false;
+                        $data = file_get_contents($_FILES[$field]['tmp_name']);
+                        if (!$data) $fields_not_empty = false;
+                        array_push($sql_fields, pg_escape_bytea($data));
+                    } else {
+                        if (empty($_POST[$field])) $fields_not_empty = false;
+                        array_push($sql_fields, $_POST[$field]);
+                    }
+                }
+
+                if ($fields_not_empty) {
+                    $query = "INSERT INTO {$_GET['table']} VALUES ('" . join("','", $sql_fields) . "')";
+                    //die($query);
                     pg_query($db, $query);
                 }
                 break;
