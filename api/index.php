45f48a9923c33e71baef85903ac0bb73ab482801
Maxim Mazurok <maxim@mazurok.com>
2018-01-05 20:48:05 +0200

    auth CORS #12

--- api/index.php
+++ api/index.php
@@ -11,10 +11,6 @@
  *
  */
 
-header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
-header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token');
-
 require_once('../vendor/autoload.php');
 if (!array_key_exists('PHP_ENV', $_ENV) || $_ENV['PHP_ENV'] !== 'production') {
     $dotenv = new Dotenv\Dotenv(__DIR__ . DIRECTORY_SEPARATOR . '..');
